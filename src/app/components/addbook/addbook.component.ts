cc2b7ef56b108fa1d2d8f602f66031af2fd0f149
Kalo <calogero.g.1996@gmail.com>
2023-06-12 17:13:14 +0200

    Sistemata addBook

--- src/app/components/addbook/addbook.component.ts
+++ src/app/components/addbook/addbook.component.ts
@@ -1,26 +1,36 @@
 import { Component, Input } from '@angular/core';
 import { ArchiveService } from 'src/app/service/archive.service';
-import { Book } from 'src/app/objects/Book';
+import { Archive } from 'src/app/objects/Archive';
 
 @Component({
   selector: 'addbook',
   templateUrl: './addbook.component.html',
-  styleUrls: ['./addbook.component.css']
+  styleUrls: ['./addbook.component.css'],
 })
 
 export class AddBookComponent {
-
   constructor(private data: ArchiveService) {}
 
-  @Input() list = new Array<Book>();
+  archive = new Archive();
 
   addBook() {
+
     let newCode = document.getElementById('newCode') as HTMLInputElement;
     let newTitle = document.getElementById('newTitle') as HTMLInputElement;
     let newAuthor = document.getElementById('newAuthor') as HTMLInputElement;
 
-    
+    if (!newCode.value || !newTitle.value || !newAuthor.value) {
+      alert('Bisogna compilare tutti i campi! Riprova.');
 
+    } else {
+      this.archive.add(newCode.value, newTitle.value, newAuthor.value);
+
+      this.data.set(JSON.stringify(this.archive)).subscribe(() => {
+        alert(
+          'Il nuovo libro con codice:' + newCode.value + ', titolo: ' +
+            newTitle.value + ' e autore: ' +
+            newAuthor.value + ' è stato inserito.');
+      });
+    }
   }
-
 }
