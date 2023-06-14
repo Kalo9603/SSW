b7f15ed3a68bf1eaf9f1fd343490c025da7b1229
Kalo <calogero.g.1996@gmail.com>
2023-06-14 17:27:22 +0200

    Implementati prestito e restituzione. Perfezionare

--- src/app/components/findbook/findbook.component.ts
+++ src/app/components/findbook/findbook.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Book } from 'src/app/objects/Book';
 import { Archive } from 'src/app/objects/Archive';
 import { ArchiveService } from 'src/app/service/archive.service';
+
 import { faMagnifyingGlass, faUser, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faUser as lightUser } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'findbook',
@@ -12,8 +14,10 @@ import { faMagnifyingGlass, faUser, faTrash } from '@fortawesome/free-solid-svg-
 
 export class FindbookComponent implements OnInit {
 
+  // Icone
   searchIcon = faMagnifyingGlass;
   userIcon = faUser;
+  lightUserIcon = lightUser;
   trashIcon = faTrash;
 
   archive = new Archive();
@@ -22,6 +26,7 @@ export class FindbookComponent implements OnInit {
   resultLen = -1;
   bookTarget = new Book('', '', '');
   isFree = this.bookTarget.isFree();
+  showBorrowFormFlag = false;
 
   constructor(private data: ArchiveService) {}
 
@@ -33,6 +38,7 @@ export class FindbookComponent implements OnInit {
 
         let code = this.archive.find(key.value)[0].getCode();
         this.bookTarget = this.archive.findBook(code);
+        this.isFree = this.bookTarget.isFree();
 
       }
     } else {
@@ -45,9 +51,31 @@ export class FindbookComponent implements OnInit {
     this.archive.remove(this.bookTarget.getCode());
     this.data.set(JSON.stringify(this.archive)).subscribe(() => {
       alert(
-        'Il nuovo libro con codice ' + this.bookTarget.getCode() + ' è stato eliminato!');
+        'Il libro con codice ' + this.bookTarget.getCode() + ' è stato eliminato!');
     });
 
+  }
+
+  showBorrowForm() {
+    this.showBorrowFormFlag = !this.showBorrowFormFlag;
+  }
+
+  borrow() {
+    let person = document.getElementById('person') as HTMLInputElement;
+    this.archive.borrow(this.bookTarget.getCode(), person.value);
+    this.data.set(JSON.stringify(this.archive)).subscribe(() => {
+      alert(
+        'Il libro con codice ' + this.bookTarget.getCode() + ' è stato preso in prestito!');
+    });
+  }
+
+  free() {
+    this.archive.free(this.bookTarget.getCode());
+    this.data.set(JSON.stringify(this.archive)).subscribe(() => {
+      alert(
+        'Il libro con codice ' + this.bookTarget.getCode() + ' è stato restituito!');
+    });
+    this.showBorrowFormFlag = false;
   }
 
   ngOnInit() {
